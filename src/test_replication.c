1e1b36e5ee3b538b9b9a559e42d5de955a9fe678
Free Ekanayaka <free.ekanayaka@canonical.com>
2018-02-19 10:55:52 +0000

    Add a stub implementation of the replication interface and initial tests
    The replication methods are currently just no-op, they will be filled
    with actual testing logic in later commits.

---
+++ src/test_replication.c
@@ -0,0 +1,144 @@
+/*
+** 2018 February 19
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+*************************************************************************
+**
+** This file contains code used for testing the SQLite system.
+** None of the code in this file goes into a deliverable build.
+**
+** This file contains a stub implementation of the write-ahead log replication
+** interface. It can be used by tests to exercise the replication APIs exposed
+** by SQLite.
+**
+** This replication implementation is designed for testability and does
+** not involve any actual networking.
+*/
+#if defined(INCLUDE_SQLITE_TCL_H)
+#  include "sqlite_tcl.h"
+#else
+#  include "tcl.h"
+#endif
+
+#if defined(SQLITE_ENABLE_REPLICATION) && !defined(SQLITE_OMIT_WAL)
+
+#include "sqliteInt.h"
+#include "sqlite3.h"
+#include <assert.h>
+
+/*
+** A no-op version of sqlite3_replication_methods.xBegin().
+*/
+static int testreplicationBegin(void *pCtx){
+  return 0;
+}
+
+/*
+** A no-op version of sqlite3_replication_methods.xAbort().
+*/
+static int testreplicationAbort(void *pCtx){
+  return 0;
+}
+
+/*
+** A no-op version of sqlite3_replication_methods.xFrames().
+*/
+static int testreplicationFrames(void *pCtx, int szPage, int nList,
+  sqlite3_replication_page *pList, unsigned nTruncate, int isCommit,
+  unsigned sync_flags
+){
+  return 0;
+}
+
+/*
+** A no-op version of sqlite3_replication_methods.xUndo().
+*/
+static int testreplicationUndo(void *pCtx){
+  return 0;
+}
+
+/*
+** A no-op version of sqlite3_replication_methods.xEnd().
+*/
+static int testreplicationEnd(void *pCtx){
+  return 0;
+}
+
+/*
+** Invoke this routine to register or unregister the stub write-ahead log
+** replication implemented by this file.
+**
+** Install the test replication if installFlag is 1 and uninstall it if
+** installFlag is 0.
+*/
+void installTestReplication(
+  int installFlag            /* True to install.  False to uninstall. */
+){
+  static const sqlite3_replication_methods testReplication = {
+    testreplicationBegin,
+    testreplicationAbort,
+    testreplicationFrames,
+    testreplicationUndo,
+    testreplicationEnd,
+  };
+  static sqlite3_replication_methods defaultReplication;
+  static int isInstalled = 0;
+
+  if( installFlag!=isInstalled ){
+    if( installFlag ){
+      sqlite3_config(SQLITE_CONFIG_GETREPLICATION, &defaultReplication);
+      assert( defaultReplication.xBegin==0 );
+      sqlite3_config(SQLITE_CONFIG_REPLICATION, &testReplication);
+    }else{
+      assert( defaultReplication.xBegin==0 );
+      sqlite3_config(SQLITE_CONFIG_REPLICATION, &defaultReplication);
+    }
+    isInstalled = installFlag;
+  }
+}
+
+/*
+** Usage:    sqlite3_config_test_replication INSTALL_FLAG
+**
+** Set up the test write-ahead log replication.  Install if INSTALL_FLAG is true
+** and uninstall (reverting to no replication at all) if INSTALL_FLAG is false.
+*/
+static int SQLITE_TCLAPI test_replication(
+  void * clientData,
+  Tcl_Interp *interp,
+  int objc,
+  Tcl_Obj *CONST objv[]
+){
+  int installFlag;
+  extern void installTestReplication(int);
+  if( objc<2 || objc>2 ){
+    Tcl_WrongNumArgs(interp, 1, objv,
+        "INSTALLFLAG");
+    return TCL_ERROR;
+  }
+  if( Tcl_GetIntFromObj(interp, objv[1], &installFlag) ) return TCL_ERROR;
+  installTestReplication(installFlag);
+
+  return TCL_OK;
+}
+
+#endif  /* SQLITE_ENABLE_REPLICATION */
+
+/*
+** This routine registers the custom TCL commands defined in this
+** module.  This should be the only procedure visible from outside
+** of this module.
+*/
+int Sqlitetestreplication_Init(Tcl_Interp *interp){
+#if defined(SQLITE_ENABLE_REPLICATION) && !defined(SQLITE_OMIT_WAL)
+  Tcl_CreateObjCommand(interp, "sqlite3_config_test_replication",
+          test_replication,0,0);
+#endif  /* SQLITE_ENABLE_REPLICATION */
+  return TCL_OK;
+}

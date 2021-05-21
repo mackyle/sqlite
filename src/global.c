b40f65e8913293145122b0d1161e47e7d5226583
D. Richard Hipp <drh@hwaci.com>
2021-05-21 13:32:10 +0000

    Add a new sqlite3_config() option for setting the maximum precision of a printf() substitition.  The default value is 100,000.  It was formerly more than 2 billion.  The default can be changed using the SQLITE_PRINTF_PRECISION_LIMIT compile-time option.

--- src/global.c
+++ src/global.c
@@ -231,64 +231,69 @@ const unsigned char sqlite3CtypeMap[256] = {
 # define SQLITE_MEMDB_DEFAULT_MAXSIZE 1073741824
 #endif
 
+/* The default maximum value for precision in printf(). */
+#ifndef SQLITE_PRINTF_PRECISION_LIMIT
+# define SQLITE_PRINTF_PRECISION_LIMIT 100000
+#endif
+
 /*
 ** The following singleton contains the global configuration for
 ** the SQLite library.
 */
 SQLITE_WSD struct Sqlite3Config sqlite3Config = {
-   SQLITE_DEFAULT_MEMSTATUS,  /* bMemstat */
-   1,                         /* bCoreMutex */
-   SQLITE_THREADSAFE==1,      /* bFullMutex */
-   SQLITE_USE_URI,            /* bOpenUri */
+   SQLITE_DEFAULT_MEMSTATUS,           /* bMemstat */
+   1,                                  /* bCoreMutex */
+   SQLITE_THREADSAFE==1,               /* bFullMutex */
+   SQLITE_USE_URI,                     /* bOpenUri */
    SQLITE_ALLOW_COVERING_INDEX_SCAN,   /* bUseCis */
-   0,                         /* bSmallMalloc */
-   1,                         /* bExtraSchemaChecks */
-   0x7ffffffe,                /* mxStrlen */
-   0,                         /* neverCorrupt */
-   SQLITE_DEFAULT_LOOKASIDE,  /* szLookaside, nLookaside */
-   SQLITE_STMTJRNL_SPILL,     /* nStmtSpill */
-   {0,0,0,0,0,0,0,0},         /* m */
-   {0,0,0,0,0,0,0,0,0},       /* mutex */
-   {0,0,0,0,0,0,0,0,0,0,0,0,0},/* pcache2 */
-   (void*)0,                  /* pHeap */
-   0,                         /* nHeap */
-   0, 0,                      /* mnHeap, mxHeap */
-   SQLITE_DEFAULT_MMAP_SIZE,  /* szMmap */
-   SQLITE_MAX_MMAP_SIZE,      /* mxMmap */
-   (void*)0,                  /* pPage */
-   0,                         /* szPage */
-   SQLITE_DEFAULT_PCACHE_INITSZ, /* nPage */
-   0,                         /* mxParserStack */
-   0,                         /* sharedCacheEnabled */
-   SQLITE_SORTER_PMASZ,       /* szPma */
-   /* All the rest should always be initialized to zero */
-   0,                         /* isInit */
-   0,                         /* inProgress */
-   0,                         /* isMutexInit */
-   0,                         /* isMallocInit */
-   0,                         /* isPCacheInit */
-   0,                         /* nRefInitMutex */
-   0,                         /* pInitMutex */
-   0,                         /* xLog */
-   0,                         /* pLogArg */
+   0,                                  /* bSmallMalloc */
+   1,                                  /* bExtraSchemaChecks */
+   0x7ffffffe,                         /* mxStrlen */
+   0,                                  /* neverCorrupt */
+   SQLITE_DEFAULT_LOOKASIDE,           /* szLookaside, nLookaside */
+   SQLITE_STMTJRNL_SPILL,              /* nStmtSpill */
+   {0,0,0,0,0,0,0,0},                  /* m */
+   {0,0,0,0,0,0,0,0,0},                /* mutex */
+   {0,0,0,0,0,0,0,0,0,0,0,0,0},        /* pcache2 */
+   (void*)0,                           /* pHeap */
+   0,                                  /* nHeap */
+   0, 0,                               /* mnHeap, mxHeap */
+   SQLITE_DEFAULT_MMAP_SIZE,           /* szMmap */
+   SQLITE_MAX_MMAP_SIZE,               /* mxMmap */
+   (void*)0,                           /* pPage */
+   0,                                  /* szPage */
+   SQLITE_DEFAULT_PCACHE_INITSZ,       /* nPage */
+   0,                                  /* mxParserStack */
+   0,                                  /* sharedCacheEnabled */
+   SQLITE_SORTER_PMASZ,                /* szPma */
+   0,                                  /* isInit */
+   0,                                  /* inProgress */
+   0,                                  /* isMutexInit */
+   0,                                  /* isMallocInit */
+   0,                                  /* isPCacheInit */
+   0,                                  /* nRefInitMutex */
+   0,                                  /* pInitMutex */
+   0,                                  /* xLog */
+   0,                                  /* pLogArg */
 #ifdef SQLITE_ENABLE_SQLLOG
-   0,                         /* xSqllog */
-   0,                         /* pSqllogArg */
+   0,                                  /* xSqllog */
+   0,                                  /* pSqllogArg */
 #endif
 #ifdef SQLITE_VDBE_COVERAGE
-   0,                         /* xVdbeBranch */
-   0,                         /* pVbeBranchArg */
+   0,                                  /* xVdbeBranch */
+   0,                                  /* pVbeBranchArg */
 #endif
 #ifndef SQLITE_OMIT_DESERIALIZE
-   SQLITE_MEMDB_DEFAULT_MAXSIZE,   /* mxMemdbSize */
+   SQLITE_MEMDB_DEFAULT_MAXSIZE,       /* mxMemdbSize */
 #endif
 #ifndef SQLITE_UNTESTABLE
-   0,                         /* xTestCallback */
+   0,                                  /* xTestCallback */
 #endif
-   0,                         /* bLocaltimeFault */
-   0x7ffffffe,                /* iOnceResetThreshold */
-   SQLITE_DEFAULT_SORTERREF_SIZE,   /* szSorterRef */
-   0,                         /* iPrngSeed */
+   0,                                  /* bLocaltimeFault */
+   0x7ffffffe,                         /* iOnceResetThreshold */
+   SQLITE_DEFAULT_SORTERREF_SIZE,      /* szSorterRef */
+   0,                                  /* iPrngSeed */
+   SQLITE_PRINTF_PRECISION_LIMIT,      /* mxPrecision */
 };
 
 /*

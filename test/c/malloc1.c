af8d1879b4e6441d6f789191df429789437d4644
Dan Kennedy <danielk1977@gmail.com>
2026-04-13 11:20:51 +0000

    Update test/c/snprintf1.c to test the result of the sqlite3_snprintf() call. Also add an SQLITE_OMIT_AUTOINIT configuration and update C tests to account for it.

--- test/c/malloc1.c
+++ test/c/malloc1.c
@@ -3,7 +3,12 @@
 #include <stdio.h>
 
 int main(void) {
-  void *p = sqlite3_malloc(32);
+  void *p = 0;
+#ifdef SQLITE_OMIT_AUTOINIT
+  sqlite3_initialize();
+#endif
+  p = sqlite3_malloc(32);
+  if( !p ) return 1;
   sqlite3_free(p);
   return 0;
 }
--- test/c/snprintf1.c
+++ test/c/snprintf1.c
@@ -2,11 +2,17 @@
 #include <stdio.h>
 
 int main(void) {
-    char szBuffer[32];
-    double val = 2023.0;
-    sqlite3_snprintf(17, szBuffer, "%.3f", val);
-    printf("size 17: '%s'\n", szBuffer);
-    sqlite3_snprintf(16, szBuffer, "%.3f", val);
-    printf("size 16: '%s'\n", szBuffer);
-    return 0;
+  const char *zExpect = "2023.000";
+  char szBuffer[32];
+  double val = 2023.0;
+#ifdef SQLITE_OMIT_AUTOINIT
+  sqlite3_initialize();
+#endif
+  sqlite3_snprintf(17, szBuffer, "%.3f", val);
+  printf("size 17: '%s'\n", szBuffer);
+  if( sqlite3_stricmp(zExpect, szBuffer) ) return 1;
+  sqlite3_snprintf(16, szBuffer, "%.3f", val);
+  printf("size 16: '%s'\n", szBuffer);
+  if( sqlite3_stricmp(zExpect, szBuffer) ) return 1;
+  return 0;
 }
